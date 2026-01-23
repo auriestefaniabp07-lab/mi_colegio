07448bb1c5ca4cf8192fbe010663e3be5c348c50
auriestefaniabp07-lab <auriestefaniabp07@gmail.com>
2026-01-23 17:45:19 -0400

    tercera parte de agregacion de css y urls

--- core/urls.py
+++ core/urls.py
@@ -4,6 +4,12 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('', views.colegio2, name='colegio2'),
+    path('colegio2', views.colegio2, name='colegio2'),
+    path('colegio3', views.colegio3, name='colegio3'),
+    path('crearregistro', views.crearregistro, name='crearregistro'),
+    path('docente_colegio', views.docente_colegio, name='docente_colegio'),
+    path('docente_colegio2', views.docente_colegio2, name='docente_colegio2'),
+    path('materia_colegio', views.materia_colegio, name='materia_colegio'),
+    path('seccion_colegio', views.seccion_colegio, name='seccion_colegio'),
     
 ]
--- core/views.py
+++ core/views.py
@@ -6,3 +6,21 @@ def index(request):
 
 def colegio2(request):
     return render(request, 'core/colegio2.html')
+
+def colegio3(request):
+    return render(request, 'core/colegio3.html')
+
+def crearregistro(request):
+    return render(request, 'core/crearregistro.html')
+
+def docente_colegio(request):
+    return render(request, 'core/docente_colegio.html')
+
+def docente_colegio2(request):
+    return render(request, 'core/docente_colegio2.html')
+
+def materia_colegio(request):
+    return render(request, 'core/materia_colegio.html')
+
+def seccion_colegio(request):
+    return render(request, 'core/seccion_colegio.html')
