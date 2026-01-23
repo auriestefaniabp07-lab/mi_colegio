6e668a3158eeb23e085e8421fc14d00dab8a5bb9
auriestefaniabp07-lab <auriestefaniabp07@gmail.com>
2026-01-23 12:39:52 -0400

    segundo parte de agregacion de css e imagenes

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('', views.colegio2, name='colegio2'),
     
 ]
--- core/views.py
+++ core/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'core/index.html')
+
+def colegio2(request):
+    return render(request, 'core/colegio2.html')
