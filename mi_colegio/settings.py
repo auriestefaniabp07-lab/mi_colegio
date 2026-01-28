57aae84a6de66413884b586780424bfbbb8194bb
auriestefaniabp07-lab <auriestefaniabp07@gmail.com>
2026-01-28 17:43:50 -0400

    cambio de html,comienzo de base de datos, herencia y usuario personalizado

--- mi_colegio/settings.py
+++ mi_colegio/settings.py
@@ -115,3 +115,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+AUTH_USER_MODEL = 'core.User'
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = BASE_DIR / 'media'
